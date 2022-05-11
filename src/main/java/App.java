53eb29f696df806326ed8e1c35918e6c84e42889
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-11 20:42:52 +0300

    Update main java class to include port number to be used by Heroku.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -20,7 +20,15 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         String connectionString = "jdbc:postgresql://localhost:5432/wildlife_tracker";//connect to wildlife_tracker, not wildlife_tracker_test!
