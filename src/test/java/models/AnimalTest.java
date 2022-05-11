7ebc1d34e7f3647c9adb24fc4f50cb34fe6ee6b6
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-11 11:01:04 +0300

    Add test to behaviours yet implemented.

--- src/test/java/models/AnimalTest.java
+++ src/test/java/models/AnimalTest.java
@@ -1,4 +1,27 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AnimalTest {
+    @Test
+    void objectInstantiatesCorrectly_True() {
+        Animal animal = new Animal("Tiger");
+        assertTrue(animal instanceof Animal);
+    }
+
+    @Test
+    void getName_returnsNameAttributeCorrectly_String() {
+        Animal animal = new Animal("Tiger");
+        assertEquals( "Tiger", animal.getName());
+    }
+
+    @Test
+    void equals_comparesContents() {
+        Animal animal = new Animal("Tiger");
+        Animal animal2 = new Animal("Tiger");
+        assertEquals(animal, animal2);
+    }
 }
