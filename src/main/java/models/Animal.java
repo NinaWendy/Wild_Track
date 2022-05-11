f33fe5248d2a52a4efc24340190b8c147f6dcf09
NinaWendy <wendy.busolo@student.moringaschool.com>
2022-05-11 11:01:42 +0300

    Implement animal getters and setters and equals method.

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,4 +1,55 @@
 package models;
 
 public class Animal {
+    protected String name;
+    protected int id;
+    protected boolean endangered;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isEndangered() {
+        return endangered;
+    }
+
+    public void setEndangered(boolean endangered) {
+        this.endangered = endangered;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Animal animal = (Animal) o;
+
+        if (id != animal.id) return false;
+        if (endangered != animal.endangered) return false;
+        return name != null ? name.equals(animal.name) : animal.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        result = 31 * result + (endangered ? 1 : 0);
+        return result;
+    }
 }
